53eb90d1357d5362d9b7185fc3a6379508f77427
negi524 <egtnrumpbiizarre.info@gmail.com>
2024-12-18 18:00:45 +0900

    Merge pull request #1 from negi524/feature/langchain
    PDF読み込み処理実装

---
+++ src/my_pdf_reader.py
@@ -0,0 +1,28 @@
+from pydantic import BaseModel
+import application_setting.my_logger as mylogger
+from logging import Logger
+from pypdf import PdfReader
+from langchain_community.document_loaders import PyPDFLoader
+
+logger: Logger = mylogger.get_logger("my_pdf_reader")
+
+
+class MyPdfReader(BaseModel):
+
+    def read_pdf(self, filepath: str) -> None:
+
+        # pypdfをシンプルに利用する
+        logger.info("pypdfをシンプルに利用してPDF読み込み")
+        reader = PdfReader(filepath)
+        number_of_pages = len(reader.pages)
+        page = reader.pages[1]
+
+        text = page.extract_text()
+        logger.info(text)
+        logger.info("========================")
+
+        # langchainを利用する
+        logger.info("langchainを利用してPDF読み込み")
+        loader = PyPDFLoader(filepath)
+        docs = loader.load()
+        logger.info(docs[1])
