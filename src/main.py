e04f5583d99ebfeb23b2e302b66cf006a01df9ae
negi524 <egtnrumpbiizarre.info@gmail.com>
2024-12-18 17:39:12 +0900

    feat: pypdfによるPDFパース

--- src/main.py
+++ src/main.py
@@ -2,6 +2,7 @@ import application_setting.my_logger as mylogger
 from logging import Logger
 import application_setting.my_credential as mycredential
 from typing import Optional
+from pypdf import PdfReader
 
 # loggerを取得
 logger: Logger = mylogger.get_logger("main")
@@ -10,6 +11,12 @@ logger: Logger = mylogger.get_logger("main")
 def main():
     logger.info("hello, world.")
     print(mycredential.KEY)
+    reader = PdfReader("./resources/気候変動対策.pdf")
+    number_of_pages = len(reader.pages)
+    page = reader.pages[0]
+
+    text = page.extract_text()
+    logger.info(text)
 
 
 def add_one(number: int) -> int:
